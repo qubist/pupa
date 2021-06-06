0c473fec5231a4e4a8acdd4457fdac00f40748e8
Will Harris-Braun <willharrisbraun@gmail.com>
2021-06-05 22:08:16 -0400

    -

--- dist/lib/pupate.js
+++ dist/lib/pupate.js
@@ -21,10 +21,12 @@ function spawn() {
         fs.mkdirSync('larva/entries');
     }
     if (!fs.existsSync("larva/" + HOMEPAGE_FILENAME)) {
-        fs.copyFileSync(path.resolve(__dirname, "./defaults/larva/" + HOMEPAGE_FILENAME), "./larva/" + HOMEPAGE_FILENAME);
+        // copy homepage file from lib/defaults/ (where all pupate default files are
+        // stored) to the working directory
+        fs.copyFileSync(path.resolve(__dirname, "../../lib/defaults/larva/" + HOMEPAGE_FILENAME), "./larva/" + HOMEPAGE_FILENAME);
     }
     if (!fs.existsSync(OPTIONS_FILENAME)) {
-        fs.copyFileSync(path.resolve(__dirname, "./defaults/" + OPTIONS_FILENAME), "./" + OPTIONS_FILENAME);
+        fs.copyFileSync(path.resolve(__dirname, "../../lib/defaults/" + OPTIONS_FILENAME), "./" + OPTIONS_FILENAME);
     }
     console.log(colors.green('Spawning finished!'));
 }
@@ -50,7 +52,8 @@ function isPupateDir() {
 // Returns a dictionary of option-name: value pairs.
 function getOptions() {
     var optionsList = fs.readFileSync(OPTIONS_FILENAME).toString().split(/\r?\n/);
-    if (!validOptions(optionsList)) {
+    // check that the options file is formatted right
+    if (!validOptionsFormat(optionsList)) {
         throw colors.red('Options file not formatted properly');
     }
     var optionsDict = {};
@@ -67,9 +70,15 @@ function getOptions() {
         }
         optionsDict[o.substr(0, o.indexOf(' '))] = o.substr(o.indexOf(' ') + 1);
     }
+    // FIXME: apply default option values to any options that are undefined
+    // check that the values of all the options are resonable
+    var result = validOptionsValues(optionsDict);
+    if (!result.boolean) {
+        throw colors.red("The following values in " + OPTIONS_FILENAME + " were not valid: " + result.details);
+    }
     return optionsDict;
 }
-function validOptions(optionsList) {
+function validOptionsFormat(optionsList) {
     for (var _i = 0, optionsList_2 = optionsList; _i < optionsList_2.length; _i++) {
         var o = optionsList_2[_i];
         // regex: each line must be either: an option name with any amount of
@@ -81,6 +90,14 @@ function validOptions(optionsList) {
     }
     return true;
 }
+// Returns whether the option values were valid and possibly a string of the the
+// options values that were invalid.
+function validOptionsValues(_options) {
+    // FIXME
+    // e.g. "sortIndexBy" has to be 'newest' 'oldest' etc.
+    // also accept undefined!
+    return { boolean: true, details: undefined };
+}
 function ecdysis() {
     console.log(colors.green('Molting...'));
     // make sure current directory is Pupate-shaped
@@ -88,15 +105,16 @@ function ecdysis() {
     // get user-defined options
     var options = getOptions();
     console.log(options);
-    // make entry objects from the text files in the entries directory
-    var entries = [];
+    // make pageEntry objects from the text files in the entries directory
+    var pageEntries = [];
     for (var _i = 0, _a = fs.readdirSync('./larva/entries').filter(isTxt); _i < _a.length; _i++) {
         var filepath = _a[_i];
-        entries.push(makeEntry("./larva/entries/" + filepath));
+        pageEntries.push(makeEntry("./larva/entries/" + filepath));
     }
+    // set output location for the finished site
     var outputLocation;
-    // default output location is imago directory, level with larva
     if (options['outputLocationIs'] == undefined) {
+        // default output location is imago directory, level with larva
         outputLocation = './imago';
     }
     else {
@@ -105,13 +123,16 @@ function ecdysis() {
     if (!fs.existsSync(outputLocation)) {
         fs.mkdirSync(outputLocation, { recursive: true });
     }
-    // create homepage
-    fs.writeFileSync(outputLocation + "/index.html", renderEntry(makeEntry('./larva/homepage.txt')));
     // create pages
-    for (var _b = 0, entries_1 = entries; _b < entries_1.length; _b++) {
-        var entry = entries_1[_b];
-        createPage(entry, outputLocation);
+    for (var _b = 0, pageEntries_1 = pageEntries; _b < pageEntries_1.length; _b++) {
+        var pageEntry = pageEntries_1[_b];
+        createPage(pageEntry, outputLocation, options);
     }
+    // create homepage
+    var homepageEntry = makeEntry('./larva/homepage.txt');
+    createHomepage(homepageEntry, outputLocation, pageEntries, options);
+    // create stylesheet
+    createStylesheet(outputLocation, options);
 }
 exports.ecdysis = ecdysis;
 function isTxt(filepath) {
@@ -129,13 +150,99 @@ function makeEntry(path) {
         content: lines.slice(2).join('\n') // all lines after that are content
     };
 }
-function createPage(entry, path) {
-    var urlPart = entry.filename;
-    fs.mkdirSync(path + "/" + urlPart, { recursive: true });
-    fs.writeFileSync(path + "/" + urlPart + "/index.html", renderEntry(entry));
+// Creates a page by rendering the page and writing it to a file inside the correct folder
+function createPage(entry, outputLocation, _options) {
+    var urlPart = entry.filename; // FIXME use option to decide
+    fs.mkdirSync(outputLocation + "/" + urlPart, { recursive: true });
+    fs.writeFileSync(outputLocation + "/" + urlPart + "/index.html", renderPage(entry));
 }
-function renderEntry(entry) {
-    console.log(entry.content);
-    return "Rendered entry: " + entry.title + ", date: " + entry.datestring + "\r\ncontent: " + entry.content + " !! :) :)";
-    // FIXME
+// Renders an Entry into a Page (html string)
+function renderPage(entry) {
+    // read default page html from defaults folder
+    var page = fs.readFileSync(path.resolve(__dirname, '../../lib/defaults/imago/page.html')).toString();
+    // replace() documentation: If pattern is a string, only the first occurrence will be replaced.
+    // replace keywords in reverse order in the template so the replaced content can't interfere with the process
+    page = page.replace(/CONTENT/, entry.content);
+    page = page.replace(/DATESTRING/, entry.datestring);
+    page = page.replace(/TITLEBODY/, entry.title);
+    page = page.replace(/TITLE/, entry.title);
+    return page;
+}
+// Creates the homepage by rendering it and outputing the file to the right location
+function createHomepage(entry, outputLocation, pageEntries, options) {
+    fs.writeFileSync(outputLocation + "/index.html", renderHomepage(entry, pageEntries, options));
+    // FIXME: BUG: doesn't overwrite old files. Delete contents of destination first? Find out how to overwrite?
+}
+// Renders an entry into the homepage (html string), ignoring datestring,
+// and creating an index from the list of page entries
+function renderHomepage(entry, pageEntries, options) {
+    // bring in template homepage from defaults folder
+    var homepage = fs.readFileSync(path.resolve(__dirname, '../../lib/defaults/imago/homepage.html')).toString();
+    // create index from list of page entries
+    var index = '';
+    if (options['showIndexWith'] != 'dont') { // if we should actually make an index
+        // decide from options how to sort the index
+        var sortFunction = void 0;
+        switch (options['sortIndexBy']) {
+            case 'newest':
+                sortFunction = function (a, b) { return a.datestring.localeCompare(b.datestring); };
+                break;
+            case 'oldest':
+                sortFunction = function (a, b) { return -1 * a.datestring.localeCompare(b.datestring); };
+                break;
+            case 'filename':
+                sortFunction = function (a, b) { return a.filename.localeCompare(b.filename); };
+                break;
+            case 'title':
+                sortFunction = function (a, b) { return a.title.localeCompare(b.title); };
+                break;
+            // validOptionsValues should catch anything else, so no default needed
+        }
+        // decide from options how to display the index
+        //  - with or without dates
+        //  - with the proper links
+        console.log(pageEntries);
+        console.log(pageEntries.sort(sortFunction));
+        for (var _i = 0, _a = pageEntries.sort(sortFunction); _i < _a.length; _i++) {
+            var entry_1 = _a[_i];
+            // get the link that will lead to the entry's page
+            var entryLink = void 0;
+            if (options['entryURLsBasedOn'] == 'title') {
+                entryLink = entry_1.title;
+            }
+            else {
+                entryLink = entry_1.filename;
+            }
+            if (options['showIndexWith'] == 'dates') {
+                index += entry_1.datestring + " <a href=\"" + entryLink + "\">" + entry_1.title + "</a>\n";
+            }
+            else if (options['showIndexWith'] == 'noDates') {
+                index += "<a href=\"" + entryLink + "\">" + entry_1.title + "</a>\n";
+            }
+            else {
+                throw colors.red('Unknown value for showIndexWith option');
+            }
+        }
+    }
+    // replace keywords in homepage template, bottom to top
+    homepage = homepage.replace(/INDEX/, index);
+    homepage = homepage.replace(/CONTENT/, entry.content);
+    homepage = homepage.replace(/TITLEBODY/, entry.title);
+    homepage = homepage.replace(/TITLE/, entry.title);
+    return homepage;
+}
+function createStylesheet(outputLocation, options) {
+    fs.writeFileSync(outputLocation + "/styles.css", renderStylesheet(outputLocation, options));
+}
+function renderStylesheet(outputLocation, options) {
+    // bring in template homepage from defaults folder
+    var stylesheet = fs.readFileSync(path.resolve(__dirname, '../../lib/defaults/imago/styles.css')).toString();
+    // replace keywords in styles template, bottom to top
+    // FIXME: options should mabe be an interface? needs work
+    stylesheet = stylesheet.replace(/LINKCOLOR/, options['linkColorIs']);
+    stylesheet = stylesheet.replace(/BACKGROUNDCOLOR/, options['backgroundColorIs']);
+    stylesheet = stylesheet.replace(/TEXTCOLOR/, options['textColorIs']);
+    stylesheet = stylesheet.replace(/FONTSIZE/, options['sizeIs']);
+    stylesheet = stylesheet.replace(/FONTFAMILY/, options['fontIs']);
+    return stylesheet;
 }
--- lib/pupate.ts
+++ lib/pupate.ts
@@ -20,10 +20,12 @@ export function spawn(): void {
     fs.mkdirSync('larva/entries')
   }
   if (!fs.existsSync(`larva/${HOMEPAGE_FILENAME}`)) {
-    fs.copyFileSync(path.resolve(__dirname, `./defaults/larva/${HOMEPAGE_FILENAME}`), `./larva/${HOMEPAGE_FILENAME}`)
+    // copy homepage file from lib/defaults/ (where all pupate default files are
+    // stored) to the working directory
+    fs.copyFileSync(path.resolve(__dirname, `../../lib/defaults/larva/${HOMEPAGE_FILENAME}`), `./larva/${HOMEPAGE_FILENAME}`)
   }
   if (!fs.existsSync(OPTIONS_FILENAME)) {
-    fs.copyFileSync(path.resolve(__dirname, `./defaults/${OPTIONS_FILENAME}`), `./${OPTIONS_FILENAME}`)
+    fs.copyFileSync(path.resolve(__dirname, `../../lib/defaults/${OPTIONS_FILENAME}`), `./${OPTIONS_FILENAME}`)
   }
 
   console.log(colors.green('Spawning finished!'))
@@ -52,7 +54,9 @@ type Options = Record<string, string | undefined>
 // Returns a dictionary of option-name: value pairs.
 function getOptions(): Options {
   let optionsList: string[] = fs.readFileSync(OPTIONS_FILENAME).toString().split(/\r?\n/)
-  if (!validOptions(optionsList)) {
+
+  // check that the options file is formatted right
+  if (!validOptionsFormat(optionsList)) {
     throw colors.red('Options file not formatted properly')
   }
   let optionsDict: Options = {}
@@ -68,10 +72,19 @@ function getOptions(): Options {
     }
     optionsDict[o.substr(0,o.indexOf(' '))] = o.substr(o.indexOf(' ')+1)
   }
+
+  // FIXME: apply default option values to any options that are undefined
+
+  // check that the values of all the options are resonable
+  let result: BooleanWithDetails = validOptionsValues(optionsDict)
+  if (!result.boolean) {
+    throw colors.red(`The following values in ${OPTIONS_FILENAME} were not valid: ${result.details}`)
+  }
+
   return optionsDict
 }
 
-function validOptions(optionsList: string[]): boolean {
+function validOptionsFormat(optionsList: string[]): boolean {
   for (const o of optionsList) {
     // regex: each line must be either: an option name with any amount of
     // whitespace after, or an option name with a space and then anything after
@@ -81,6 +94,20 @@ function validOptions(optionsList: string[]): boolean {
     }
   }
   return true
+}
+
+interface BooleanWithDetails {
+  boolean: boolean,
+  details: string | undefined
+}
+
+// Returns whether the option values were valid and possibly a string of the the
+// options values that were invalid.
+function validOptionsValues(_options: Options): BooleanWithDetails {
+  // FIXME
+  // e.g. "sortIndexBy" has to be 'newest' 'oldest' etc.
+  // also accept undefined!
+  return {boolean: true, details: undefined}
 }
 
 interface Entry {
@@ -100,16 +127,16 @@ export function ecdysis(): void {
   const options = getOptions()
   console.log(options)
 
-  // make entry objects from the text files in the entries directory
-  let entries: Entry[] = []
+  // make pageEntry objects from the text files in the entries directory
+  let pageEntries: Entry[] = []
   for (const filepath of fs.readdirSync('./larva/entries').filter(isTxt)) {
-    entries.push(makeEntry(`./larva/entries/${filepath}`))
+    pageEntries.push(makeEntry(`./larva/entries/${filepath}`))
   }
 
+  // set output location for the finished site
   let outputLocation: string
-
-  // default output location is imago directory, level with larva
   if (options['outputLocationIs'] == undefined) {
+    // default output location is imago directory, level with larva
     outputLocation = './imago'
   } else {
     outputLocation = options['outputLocationIs']
@@ -119,13 +146,17 @@ export function ecdysis(): void {
     fs.mkdirSync(outputLocation, {recursive: true})
   }
 
-  // create homepage
-  fs.writeFileSync(`${outputLocation}/index.html`, renderEntry(makeEntry('./larva/homepage.txt')))
-
   // create pages
-  for (const entry of entries) {
-    createPage(entry, outputLocation)
+  for (const pageEntry of pageEntries) {
+    createPage(pageEntry, outputLocation, options)
   }
+
+  // create homepage
+  let homepageEntry = makeEntry('./larva/homepage.txt')
+  createHomepage(homepageEntry, outputLocation, pageEntries, options)
+
+  // create stylesheet
+  createStylesheet(outputLocation, options)
 }
 
 function isTxt(filepath: string): boolean {
@@ -146,14 +177,109 @@ function makeEntry(path: string): Entry {
   }
 }
 
-function createPage(entry: Entry, path: string): void {
-  let urlPart: string = entry.filename
-  fs.mkdirSync(`${path}/${urlPart}`, {recursive: true})
-  fs.writeFileSync(`${path}/${urlPart}/index.html`, renderEntry(entry))
+// Creates a page by rendering the page and writing it to a file inside the correct folder
+function createPage(entry: Entry, outputLocation: string, _options: Options): void {
+  let urlPart: string = entry.filename // FIXME use option to decide
+
+  fs.mkdirSync(`${outputLocation}/${urlPart}`, {recursive: true})
+  fs.writeFileSync(`${outputLocation}/${urlPart}/index.html`, renderPage(entry))
 }
 
-function renderEntry(entry: Entry): string {
-  console.log(entry.content)
-  return `Rendered entry: ${entry.title}, date: ${entry.datestring}\r\ncontent: ${entry.content} !! :) :)`
-  // FIXME
+// Renders an Entry into a Page (html string)
+function renderPage(entry: Entry): string {
+  // read default page html from defaults folder
+  let page: string = fs.readFileSync(path.resolve(__dirname, '../../lib/defaults/imago/page.html')).toString()
+
+  // replace() documentation: If pattern is a string, only the first occurrence will be replaced.
+  // replace keywords in reverse order in the template so the replaced content can't interfere with the process
+  page = page.replace(/CONTENT/, entry.content)
+  page = page.replace(/DATESTRING/, entry.datestring)
+  page = page.replace(/TITLEBODY/, entry.title)
+  page = page.replace(/TITLE/, entry.title)
+
+  return page
+}
+
+// Creates the homepage by rendering it and outputing the file to the right location
+function createHomepage(entry: Entry, outputLocation: string, pageEntries: Entry[], options: Options): void {
+  fs.writeFileSync(`${outputLocation}/index.html`, renderHomepage(entry, pageEntries, options))
+}
+
+// Renders an entry into the homepage (html string), ignoring datestring,
+// and creating an index from the list of page entries
+function renderHomepage(entry: Entry, pageEntries: Entry[], options: Options): string {
+  // bring in template homepage from defaults folder
+  let homepage: string = fs.readFileSync(path.resolve(__dirname, '../../lib/defaults/imago/homepage.html')).toString()
+
+  // create index from list of page entries
+  let index: string = ''
+  if (options['showIndexWith'] != 'dont') { // if we should actually make an index
+    // decide from options how to sort the index
+    let sortFunction
+    switch (options['sortIndexBy']) {
+      case 'newest':
+        sortFunction = (a: Entry, b: Entry): number => a.datestring.localeCompare(b.datestring)
+        break
+      case 'oldest':
+        sortFunction = (a: Entry, b: Entry): number => -1 * a.datestring.localeCompare(b.datestring)
+        break
+      case 'filename':
+        sortFunction = (a: Entry, b: Entry): number => a.filename.localeCompare(b.filename)
+        break
+      case 'title':
+        sortFunction = (a: Entry, b: Entry): number => a.title.localeCompare(b.title)
+        break
+      // validOptionsValues should catch anything else, so no default needed
+    }
+
+    // decide from options how to display the index
+    //  - with or without dates
+    //  - with the proper links
+    console.log(pageEntries)
+    console.log(pageEntries.sort(sortFunction))
+    for (const entry of pageEntries.sort(sortFunction)) {
+      // get the link that will lead to the entry's page
+      let entryLink
+      if (options['entryURLsBasedOn'] == 'title') {
+        entryLink = entry.title
+      } else {
+        entryLink = entry.filename
+      }
+
+      if (options['showIndexWith'] == 'dates') {
+        index += `${entry.datestring} <a href="${entryLink}">${entry.title}</a>\n`
+      } else if (options['showIndexWith'] == 'noDates') {
+        index += `<a href="${entryLink}">${entry.title}</a>\n`
+      } else {
+        throw colors.red('Unknown value for showIndexWith option')
+      }
+    }
+  }
+
+  // replace keywords in homepage template, bottom to top
+  homepage = homepage.replace(/INDEX/, index)
+  homepage = homepage.replace(/CONTENT/, entry.content)
+  homepage = homepage.replace(/TITLEBODY/, entry.title)
+  homepage = homepage.replace(/TITLE/, entry.title)
+
+  return homepage
+}
+
+function createStylesheet(outputLocation: string, options: Options) {
+  fs.writeFileSync(`${outputLocation}/styles.css`, renderStylesheet(outputLocation, options))
+}
+
+function renderStylesheet(outputLocation: string, options: Options): string {
+  // bring in template homepage from defaults folder
+  let stylesheet: string = fs.readFileSync(path.resolve(__dirname, '../../lib/defaults/imago/styles.css')).toString()
+
+  // replace keywords in styles template, bottom to top
+  // FIXME: options should mabe be an interface? needs work
+  stylesheet = stylesheet.replace(/LINKCOLOR/, options['linkColorIs'])
+  stylesheet = stylesheet.replace(/BACKGROUNDCOLOR/, options['backgroundColorIs'])
+  stylesheet = stylesheet.replace(/TEXTCOLOR/, options['textColorIs'])
+  stylesheet = stylesheet.replace(/FONTSIZE/, options['sizeIs'])
+  stylesheet = stylesheet.replace(/FONTFAMILY/, options['fontIs'])
+
+  return stylesheet
 }
