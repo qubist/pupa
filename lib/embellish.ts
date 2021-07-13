9393a4d201bb16ba25080a492f3d10a053990c85
Will Harris-Braun <willharrisbraun@gmail.com>
2021-07-12 22:09:24 -0400

    Modify regex to catch CSS var() nested inside color embellish's parens

--- lib/embellish.ts
+++ lib/embellish.ts
@@ -5,6 +5,6 @@ export function embellish(text: string): string {
   return text
     .replace(/\*(\S(.*?\S)?)\*/isg, "<b>$1</b>")
     .replace(/\_(\S(.*?\S)?)\_/isg, "<i>$1</i>")
-    .replace(/{([^{]*?)}\((.*?)\)/isg, '<span style="color: $2">$1</span>')
+    .replace(/{([^{]*?)}\(([^()]*?|var\(--[_a-zA-Z0-9-]*\))\)/isg, '<span style="color: $2">$1</span>')
     .replace(/\[([^\[]+)\]\(([^\)]+)\)/isg, '<a href="$2">$1</a>')
 }
