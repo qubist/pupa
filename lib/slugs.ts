e09cb9a1c9d53fb6d527e8740905e56964a1dfef
Will Harris-Braun <willharrisbraun@gmail.com>
2021-07-25 21:07:42 -0400

    Make empty slug error message readable when bad value was empty string

--- lib/slugs.ts
+++ lib/slugs.ts
@@ -10,7 +10,7 @@ function slugify(value: string, form: PageURLsBasedOnValue): string {
   .replace(/-+/g, '-') // collapse dashes
 
   if (value == '') {
-    throw 'Slugification failed! Value '.red + value.reset + ' produced an empty slug. Try changing the '.red + form.red + ' that matches this value or choose a new option for the pageURLsBasedOn setting.'.red
+    throw 'Slugification failed! Value '.red + `"${value}"`.reset + ' produced an empty slug. Try changing the '.red + form.red + ' that matches this value or choose a new option for the pageURLsBasedOn setting.'.red
   }
 
   return value
