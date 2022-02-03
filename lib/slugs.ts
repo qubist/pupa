0d0635da6dfef6fd0ed0d52cbfb1977b7a11f182
Will Harris-Braun <willharrisbraun@gmail.com>
2022-02-03 16:37:54 -0500

    Clarify code that decides what to base slugs on

--- lib/slugs.ts
+++ lib/slugs.ts
@@ -22,13 +22,13 @@ export function getSlug(entry: Entry, basedOn: PageURLsBasedOnValue): string {
   let slug: string
   switch (basedOn) {
     case 'title':
-      slug = slugify(unembellish(entry.title), basedOn)
+      slug = slugify(unembellish(entry.title), 'title')
       break
     case 'filename':
-      slug = slugify(entry.filename, basedOn)
+      slug = slugify(entry.filename, 'filename')
       break
     case 'date':
-      slug = slugify(entry.datestring, basedOn)
+      slug = slugify(entry.datestring, 'date')
       break
     // Can't be anything else
   }
