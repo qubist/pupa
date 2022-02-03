e7814411956682939bfea5918d6667e9535a72c0
Will Harris-Braun <willharrisbraun@gmail.com>
2022-02-03 16:37:16 -0500

    Make slugification failure error message clearer

--- lib/slugs.ts
+++ lib/slugs.ts
@@ -12,7 +12,7 @@ function slugify(value: string, form: PageURLsBasedOnValue): string {
   .replace(/^-|-$/g, '') // remove leading/trailing dashes
 
   if (value == '') {
-    throw 'Slugification failed! Value '.red + `"${value}"`.reset + ' produced an empty slug. Try changing the '.red + form.red + ' that matches this value or choose a new option for the pageURLsBasedOn setting.'.red
+    throw 'Slugification failed! An entry had the '.red + form.red + ` "${value}"`.reset + ', which produced an empty slug. Try changing this '.red + form.red + ' or choose a new option for the pageURLsBasedOn setting.'.red
   }
 
   return value
