44a078ca43b29d6612aa80458d30f6176965e4c3
Will Harris-Braun <willharrisbraun@gmail.com>
2022-02-28 14:17:02 -0500

    Format string with quote better

--- bin/index.ts
+++ bin/index.ts
@@ -14,7 +14,7 @@ try {
   fs.accessSync(path.resolve(__dirname), fs.constants.R_OK | fs.constants.W_OK);
   // FIXME: check that output location is writeable too?
 } catch {
-  console.warn('WARNING! Pupate creates and deletes files in the output location, and might not run correctly if it doesn\'t have with the proper permissions'.yellow)
+  console.warn("WARNING! Pupate creates and deletes files in the output location, and might not run correctly if it doesn't have with the proper permissions".yellow)
 }
 
 let subcommand = process.argv.splice(2)[0]
