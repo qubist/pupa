93ca6d57062d5dba9698d0beb9ded3198d48d60b
Will Harris-Braun <willharrisbraun@gmail.com>
2022-03-01 21:29:09 -0500

    Use yargs package to manage CLI subcommands and options

--- bin/index.ts
+++ bin/index.ts
@@ -3,13 +3,15 @@
 import * as fs from 'fs'
 import * as path from 'path'
 
+import yargs = require('yargs')
+
 import { check, spawn, eclose } from '../lib/pupate'
 import 'colors'
 
-// print welcome
+// Print welcome
 console.log('Welcome to Pupate!'.blue)
 
-// check some permissions
+// Check read and write permissions for the pupate Directory
 try {
   fs.accessSync(path.resolve(__dirname), fs.constants.R_OK | fs.constants.W_OK);
   // FIXME: check that output location is writeable too?
@@ -17,20 +19,42 @@ try {
   console.warn("WARNING! Pupate creates and deletes files in the output location, and might not run correctly if it doesn't have with the proper permissions".yellow)
 }
 
-let subcommand = process.argv.splice(2)[0]
-
-switch (subcommand) {
-  case 'check':
-    // Check if directory is Pupate-shaped with param quiet = false
-    check(false)
-    break
-  case 'spawn':
-    spawn()
-    break
-  case undefined:
-  case 'eclose':
-    eclose()
-    break
-  default:
-    console.error('Unrecognized subcommand:'.red, subcommand)
-}
+// Handle arguments and options
+yargs
+  .command(
+    'check',
+    'check that the current working directory is a valid Pupate-shaped directory',
+    {},
+    function(argv) {
+      // Check if directory is Pupate-shaped with param quiet = false
+      check(false)
+    })
+  .command(
+    'spawn',
+    'create necessary pupate files in the current working directory',
+    {},
+    function(argv) {
+      spawn()
+    })
+  .command(
+    ['eclose'], 
+    'build the site in the output directory based on the content of the larva directory',
+    {},
+    function(argv) {
+      eclose()
+    })
+  .command(
+    '*',
+    false,
+    {},
+    function(argv) {
+      // No subcommand defaults to eclose
+      if (argv._[0] == undefined) {
+        eclose()
+      } else {
+        console.error('Unrecognized subcommand:'.red, argv._[0])
+        process.exit(1)
+      }
+    })
+  .help()
+  .argv
