ed722ce5885d5b12f9b689a3a17f2f5e7b7977b6
Will Harris-Braun <willharrisbraun@gmail.com>
2022-03-11 13:13:11 -0500

    Fix bold embelisment multi-line issue

--- lib/embellish.ts
+++ lib/embellish.ts
@@ -2,7 +2,7 @@
 // Modifies text, adding HTML in place of Pupate embellishment markup
 export function embellish(text: string): string {
   return text
-    .replace(/\*(\S([^*]*?\S)?)\*/ig, "<b>$1</b>")
+    .replace(/\*(\S([^*]*?\S)?)\*/isg, "<b>$1</b>")
     .replace(/\_(\S([^_]*?\S)?)\_/isg, "<i>$1</i>")
     .replace(/{([^{]*?)}\(([^()]*?|var\(--[_a-zA-Z0-9-]*\))\)/isg, '<span style="color: $2">$1</span>')
     .replace(/\[([^\[]+)\]\(([^\)]+)\)/isg, '<a href="$2">$1</a>')
