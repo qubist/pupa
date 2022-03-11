bfd0eac86b53c333da5bf00a22cf182d4218f7eb
Will Harris-Braun <willharrisbraun@gmail.com>
2022-03-11 13:20:54 -0500

    Standardize quotes in embellishment code

--- lib/embellish.ts
+++ lib/embellish.ts
@@ -2,8 +2,8 @@
 // Modifies text, adding HTML in place of Pupate embellishment markup
 export function embellish(text: string): string {
   return text
-    .replace(/\*(\S([^*]*?\S)?)\*/isg, "<b>$1</b>")
-    .replace(/\_(\S([^_]*?\S)?)\_/isg, "<i>$1</i>")
+    .replace(/\*(\S([^*]*?\S)?)\*/isg, '<b>$1</b>')
+    .replace(/\_(\S([^_]*?\S)?)\_/isg, '<i>$1</i>')
     .replace(/{([^{]*?)}\(([^()]*?|var\(--[_a-zA-Z0-9-]*\))\)/isg, '<span style="color: $2">$1</span>')
     .replace(/\[([^\[]+)\]\(([^\)]+)\)/isg, '<a href="$2">$1</a>')
 }
@@ -12,8 +12,8 @@ export function embellish(text: string): string {
 // so the text can be used as plain text, e.g. the document title
 export function unembellish(text: string): string {
   return text
-    .replace(/\*(\S([^*]*?\S)?)\*/isg, "$1")
-    .replace(/\_(\S([^_]*?\S)?)\_/isg, "$1")
+    .replace(/\*(\S([^*]*?\S)?)\*/isg, '$1')
+    .replace(/\_(\S([^_]*?\S)?)\_/isg, '$1')
     .replace(/{([^{]*?)}\(([^()]*?|var\(--[_a-zA-Z0-9-]*\))\)/isg, '$1')
     .replace(/\[([^\[]+)\]\(([^\)]+)\)/isg, '$1')
 }
